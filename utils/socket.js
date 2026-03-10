2d93e3f0daa26bfc2db29874e9ba7e099695b4f7
REAGANOTEMA <reaganotema2022@gmail.com>
2026-03-10 16:59:07 +0300

    initial changes

--- utils/socket.js
+++ utils/socket.js
@@ -4,18 +4,50 @@ let io;
 
 export const initSocket = (server) => {
   io = new Server(server, {
-    cors: { origin: "*", methods: ["GET", "POST"] },
+    cors: {
+      origin: "https://ncmp-ug-connect.onrender.com", // <-- your frontend
+      methods: ["GET", "POST"],
+    },
   });
 
   io.on("connection", (socket) => {
     console.log("⚡️ New client connected:", socket.id);
 
-    socket.on("join_room", (roomId) => socket.join(roomId));
-    socket.on("send_message", (data) => io.to(data.roomId).emit("receive_message", data.message));
-    socket.on("typing", (data) => socket.to(data.roomId).emit("typing", { userId: data.userId }));
-    socket.on("message_read", (data) => io.to(data.roomId).emit("message_read", { messageId: data.messageId }));
+    // Messaging
+    socket.on("join_room", (roomId) => {
+      socket.join(roomId);
+      console.log(`User joined room: ${roomId}`);
+    });
 
-    socket.on("disconnect", () => console.log("❌ Client disconnected:", socket.id));
+    socket.on("send_message", (data) => {
+      const { roomId, message } = data;
+      io.to(roomId).emit("receive_message", message);
+    });
+
+    socket.on("typing", (data) => {
+      const { roomId, userId } = data;
+      socket.to(roomId).emit("typing", { userId });
+    });
+
+    socket.on("message_read", (data) => {
+      const { roomId, messageId } = data;
+      io.to(roomId).emit("message_read", { messageId });
+    });
+
+    // Audio/Video Calls
+    socket.on("call_user", (data) => {
+      const { userToCall, signalData, from, name } = data;
+      io.to(userToCall).emit("incoming_call", { signal: signalData, from, name });
+    });
+
+    socket.on("answer_call", (data) => {
+      const { to, signal } = data;
+      io.to(to).emit("call_accepted", signal);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("❌ Client disconnected:", socket.id);
+    });
   });
 };
 
